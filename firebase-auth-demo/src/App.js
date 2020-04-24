a3d350da01eb40feeffddf915a6f16dadaed0b3a
rand0819 <robertwinstonanderson@gmail.com>
2020-04-24 15:34:50 -0600

    add PrivateRoute.js, updated App.js

--- firebase-auth-demo/src/App.js
+++ firebase-auth-demo/src/App.js
@@ -4,17 +4,20 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import { AuthProvider } from './Auth';
 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
